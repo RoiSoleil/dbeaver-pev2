98d6a72c3810ed5a5dda42e0fda8e5938ef5fedc
Hélios GILLES <helios.gilles@gmail.com>
2026-07-08 18:20:54 +0200

    Open DBeaver perspective in test setup, add required OSGi deps

--- bundles/org.eclipse.dbeaver-pev2.tests/src/org/eclipse/dbeaver_pev2/tests/AbstractSWTBotTest.java
+++ bundles/org.eclipse.dbeaver-pev2.tests/src/org/eclipse/dbeaver_pev2/tests/AbstractSWTBotTest.java
@@ -3,7 +3,10 @@ package org.eclipse.dbeaver_pev2.tests;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestWatcher;
@@ -30,6 +33,22 @@ public abstract class AbstractSWTBotTest {
     @Before
     public void setup() {
         bot = new SWTWorkbenchBot();
+        openDBeaverPerspective();
+    }
+
+    private void openDBeaverPerspective() {
+        Display.getDefault().syncExec(() -> {
+            try {
+                IWorkbenchWindow window = PlatformUI.getWorkbench()
+                    .getActiveWorkbenchWindow();
+                if (window != null) {
+                    PlatformUI.getWorkbench().showPerspective(
+                        "org.jkiss.dbeaver.core.perspective", window);
+                }
+            } catch (Exception e) {
+                // Perspective might not be available; tests will handle gracefully
+            }
+        });
     }
 
 }
