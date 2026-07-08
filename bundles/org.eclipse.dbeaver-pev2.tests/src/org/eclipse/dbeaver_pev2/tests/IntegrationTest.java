f45b6017ecce83532142dbe8c24404a7481d9b3b
Hélios GILLES <helios.gilles@gmail.com>
2026-07-08 23:18:56 +0200

    Add IntegrationTest: PostgreSQL connection, EXPLAIN query, and PEV2 editor verification

---
+++ bundles/org.eclipse.dbeaver-pev2.tests/src/org/eclipse/dbeaver_pev2/tests/IntegrationTest.java
@@ -0,0 +1,46 @@
+package org.eclipse.dbeaver_pev2.tests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.dbeaver_pev2.PEV2EditorPart;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.junit.Test;
+
+public class IntegrationTest extends AbstractSWTBotTest {
+
+    @Test
+    public void testExplainOnPostgreSQL() throws Exception {
+        PostgreSQLConnectionHelper.createConnection(bot);
+
+        bot.sleep(5000);
+
+        SWTBotTree tree = bot.viewByTitle("Database Navigator").bot().tree();
+        SWTBotTreeItem connection = tree.getAllItems()[0];
+        connection.select();
+
+        bot.menu("Navigate").menu("Open SQL Editor").click();
+        bot.sleep(3000);
+
+        bot.styledText(0).setText("EXPLAIN (FORMAT JSON) SELECT * FROM person");
+        bot.sleep(1000);
+
+        bot.toolbarButtonWithTooltip("Execute SQL Statement").click();
+        bot.sleep(5000);
+
+        bot.toolbarButtonWithTooltip("Open explain plan in PEV2").click();
+        bot.sleep(3000);
+
+        IWorkbenchPage page = PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getActivePage();
+        IEditorPart editor = page.getActiveEditor();
+        assertNotNull("PEV2 editor should be active", editor);
+        assertTrue("Editor should be PEV2EditorPart",
+            editor instanceof PEV2EditorPart);
+    }
+
+}
